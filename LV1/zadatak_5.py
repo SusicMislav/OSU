9110a134b215834e9ee0d14eb6b46fad3feffb29
SusicMislav <mislavsusic@gmail.com>
2026-04-26 23:08:24 +0200

    Update zadatak_5.py

--- LV1/zadatak_5.py
+++ LV1/zadatak_5.py
@@ -1,12 +1,14 @@
-fhand = open("zadaci/SMSSpamCollection.txt")
+fhand = open("SMSSpamCollection.txt",  encoding="utf-8")
 
 HamWordCounter = 0
 HamSMSCounter = 0
 SpamWordCounter = 0
 SpamSMSCounter = 0
-SpamSMSwithEclamation = 0
-for line in fhand :
+SpamSMSwithQuestion = 0
+for line in fhand:
     words = line.split()
+
+
     if words[0] == "ham":
         HamSMSCounter += 1
         for word in words[1::1]:
@@ -15,10 +17,10 @@ for line in fhand :
         SpamSMSCounter +=1
         for word in words[1::1]:
             SpamWordCounter += 1
-        if words.pop() == "!":
-            SpamSMSwithEclamation += 1
+        if words[-1].endswith("?"):      #You can get the last elemnt of a 
+            SpamSMSwithQuestion += 1   #list with listName[-1]
 
 print("Average number of words in ham messages: ", HamWordCounter / HamSMSCounter)
 print("Average number of words in spam messages: ", SpamWordCounter / SpamSMSCounter)
 
-print("Number of messages that finish with an exclamation mark: ", SpamSMSwithEclamation)
+print("Number of messages that finish with an question mark: ", SpamSMSwithQuestion)

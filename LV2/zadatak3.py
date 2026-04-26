47e10c0c13c363c7588036a3a7b89d2640725c27
SusicMislav <mislavsusic@gmail.com>
2026-04-26 23:45:59 +0200

    Update zadatak3.py

--- LV2/zadatak3.py
+++ LV2/zadatak3.py
@@ -6,11 +6,39 @@ img = plt.imread ("road.jpg ")
 img = img [:,:,0]. copy ()
 
 img = img.astype(np.float64)
-brighterImg = img+80
-brighterImg = np.clip(img, 0, 255)
 
-print ( brighterImg . shape )
-print ( brighterImg . dtype )
-plt . figure ()
-plt . imshow (brighterImg, cmap='gray')
-plt . show ()
+# Brighter image
+brighterImg = img+80
+brighterImg = np.clip(brighterImg, 0, 255)
+
+
+# b) DRUGA ČETVRTINA PO ŠIRINI
+h, w = img.shape
+second_quarter = img[:, w//4:w//2]
+
+# c) ROTACIJA 90° U SMJERU KAZALJKE
+rotated = np.rot90(img, k=-1)
+
+# d) ZRCALJENJE (horizontalno)
+mirrored = np.fliplr(img)
+
+# PRIKAZ
+plt.figure(figsize=(10,8))
+
+plt.subplot(2,2,1)
+plt.title("Posvijetljena")
+plt.imshow(brighterImg, cmap='gray')
+
+plt.subplot(2,2,2)
+plt.title("2. četvrtina")
+plt.imshow(second_quarter, cmap='gray')
+
+plt.subplot(2,2,3)
+plt.title("Rotirana")
+plt.imshow(rotated, cmap='gray')
+
+plt.subplot(2,2,4)
+plt.title("Zrcaljena")
+plt.imshow(mirrored, cmap='gray')
+
+plt.show()

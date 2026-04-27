4bd92def34a6bd4b642afc51ab4f748ceb018531
SusicMislav <mislavsusic@gmail.com>
2026-04-27 09:34:46 +0200

    Update zadatak1.py

--- LV4/zadatak1.py
+++ LV4/zadatak1.py
@@ -20,20 +20,20 @@ y=data[CO2_emissions]
 
 X_train , X_test , y_train , y_test = train_test_split (x, y, test_size = 0.2, random_state =1)
 
-odabrana_velicina = 'Cylinders' 
+odabrana_velicina = 'Engine Size (L)' 
 
-# plt.figure()
+plt.figure()
 
-# plt.scatter(X_train[odabrana_velicina], y_train, color='blue', label='Skup za učenje', alpha=0.5)
-# plt.scatter(X_test[odabrana_velicina], y_test, color='red', label='Skup za testiranje', alpha=0.5)
+plt.scatter(X_train[odabrana_velicina], y_train, color='blue', label='Skup za učenje', alpha=0.5)
+plt.scatter(X_test[odabrana_velicina], y_test, color='red', label='Skup za testiranje', alpha=0.5)
 
-# plt.title(f'Ovisnost emisije CO2 o {odabrana_velicina}')
-# plt.xlabel(odabrana_velicina)
-# plt.ylabel('CO2 Emissions (g/km)')
-# plt.legend()
-# plt.grid(True)
+plt.title(f'Ovisnost emisije CO2 o {odabrana_velicina}')
+plt.xlabel(odabrana_velicina)
+plt.ylabel('CO2 Emissions (g/km)')
+plt.legend()
+plt.grid(True)
 
-# plt.show()
+plt.show()
 
 
 
@@ -47,21 +47,21 @@ X_test_n = sc.transform( X_test )
 X_train_scaled_df = pd.DataFrame(X_train_n, columns=X_train.columns)
 poslije_skaliranja = X_train_scaled_df[odabrana_velicina]
 
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
-# # Histogram prije skaliranja
-# ax1.hist(prije_skaliranja, bins=20, color='skyblue', edgecolor='black')
-# ax1.set_title(f'Prije skaliranja\n({odabrana_velicina})')
-# ax1.set_xlabel('Originalna vrijednost')
-# ax1.set_ylabel('Frekvencija')
+# Histogram prije skaliranja
+ax1.hist(prije_skaliranja, bins=20, color='skyblue', edgecolor='black')
+ax1.set_title(f'Prije skaliranja\n({odabrana_velicina})')
+ax1.set_xlabel('Originalna vrijednost')
+ax1.set_ylabel('Frekvencija')
 
-# # Histogram nakon skaliranja
-# ax2.hist(poslije_skaliranja, bins=20, color='salmon', edgecolor='black')
-# ax2.set_title(f'Nakon skaliranja\n({odabrana_velicina})')
-# ax2.set_xlabel('Standardizirana vrijednost (z-score)')
+# Histogram nakon skaliranja
+ax2.hist(poslije_skaliranja, bins=20, color='salmon', edgecolor='black')
+ax2.set_title(f'Nakon skaliranja\n({odabrana_velicina})')
+ax2.set_xlabel('Standardizirana vrijednost (0-1)')
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
 
 
 linearModel = lm.LinearRegression ()
@@ -75,15 +75,24 @@ MAE = mean_absolute_error (y_test, y_test_p)
 MSE = mean_squared_error (y_test, y_test_p)
 RMSE = root_mean_squared_error (y_test, y_test_p)
 
-# plt.figure()
-# plt.scatter(y_test, y_test_p, color='darkcyan', alpha=0.6, edgecolors='k')
+# e) Izvršite procjenu izlazne veliˇcine na temelju ulaznih velicina skupa za testiranje. Prikažite pomocu dijagrama raspršenja odnos izmedu stvarnih vrijednosti izlazne velicine i procjene dobivene modelom.
 
-# plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2, label='Idealno')
+plt.figure()
+plt.scatter(y_test, y_test_p, color='darkcyan', alpha=0.6, edgecolors='k')
 
-# plt.title('Odnos stvarnih i procjenjenih vrijednosti CO2')
-# plt.xlabel('Stvarne vrijednosti (y_test)')
-# plt.ylabel('Procjene modela (y_pred)')
-# plt.legend()
-# plt.grid(True)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2, label='Idealno')
 
-# plt.show()
+plt.title('Odnos stvarnih i procjenjenih vrijednosti CO2')
+plt.xlabel('Stvarne vrijednosti (y_test)')
+plt.ylabel('Procjene modela (y_pred)')
+plt.legend()
+plt.grid(True)
+
+plt.show()
+
+
+# f) ispis metrika 
+
+print(f"MAE: {MAE}")
+print(f"MSE: {MSE}")
+print(f"RMSE: {RMSE}")
